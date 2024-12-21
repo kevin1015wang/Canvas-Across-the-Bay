63f5faa14ca65e9c2dd599d459784a25ae485064
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2024-12-21 03:22:24 +0000

    Omg finally got a map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import MapLayer from './components/mapLayer'
 
 const center = [51.505, -0.09]
 
@@ -9,12 +10,7 @@ const center = [51.505, -0.09]
 function App() {
   return (
     <div id="map">
-      <MapContainer center={center} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+      <MapLayer />
     </div>
   );
 }

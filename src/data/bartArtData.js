eadea2189c9e0cd3207c5c5323bb9596fbf98eed
Kevin Wang <38193800+kevin1015wang@users.noreply.github.com>
2024-12-23 23:21:29 +0000

    Added some art wooo

--- src/data/bartArtData.js
+++ src/data/bartArtData.js
@@ -34,6 +34,97 @@ const bartArtData = [
         artistName: "Victor Mario Zaballa",
         location: "SW & NE Plazas"
     },
+    {
+        name: "Home",
+        image: "home.jpg",
+        stationLocation: "16th St./Mission",
+        artistName: `Louis "Cork" Marcheschi`,
+        location: "NE Plaza - Elevator enclosure"
+    },
+    {
+        name: "Youth Voices",
+        image: "youth-voices.jpg",
+        stationLocation: "16th St./Mission",
+        artistName: `Gary Carlos and students from St. John's Education Threshhold Center`,
+        location: "NW Plaza - Elevator Enclosure below glass artwork"
+    },
+    {
+        name: "Unknown",
+        image: "unknown-24th.jpg",
+        stationLocation: "24th St./Mission",
+        artistName: `William Mitchell`,
+        location: "Station entrances Concourse Level"
+    },
+    {
+        name: "A-Round Oakland",
+        image: "a-round-oakland.jpg",
+        stationLocation: "Oakland Airport Connector - Coliseum Station",
+        artistName: `Gordon Huether`,
+        location: "Train Access area"
+    },
+    {
+        name: "Periodic Motion",
+        image: "periodic-motion.jpg",
+        stationLocation: "Oakland Airport Connector - Airport",
+        artistName: `Tom Patti`,
+        location: "Train Access area"
+    },
+    {
+        name: "Leonardo's Dream",
+        image: "leonardos-dream.jpg",
+        stationLocation: "Colma",
+        artistName: `Daniel Goldstein`,
+        location: "Concourse - Suspended above non-paid area"
+    },
+    {
+        name: "Arco Iris",
+        image: "arco-iris.jpg",
+        stationLocation: "Colma",
+        artistName: `Michael Hayden`,
+        location: "Platform 1 & 2- Train Load/Unload Area"
+    },
+    {
+        name: "Migration",
+        image: "migration.jpg",
+        stationLocation: "Daly City",
+        artistName: `Eric Powell`,
+        location: "St. Charles Ave. - 2 at entrance of garage, one across street from garage, and one across bridge"
+    },
+    {
+        name: "Unknown",
+        image: "unknown-elcerrito-norte.jpg",
+        stationLocation: "El Cerrito del Norte",
+        artistName: `Alfonso Pardiñas`,
+        location: "Concourse - Main Level"
+    },
+    {
+        name: "Unknown",
+        image: "unknown-elcerrito-plaza.jpg",
+        stationLocation: "El Cerrito Plaza",
+        artistName: `Alfonso Pardiñas`,
+        location: "Concourse - Main Level"
+    },
+    {
+        name: "M.J.K.J.J.",
+        image: "mjkjj.jpg",
+        stationLocation: "Embarcadero",
+        artistName: `Doug Heine`,
+        location: "Concourse - Main Level"
+    },
+    {
+        name: "Spin",
+        image: "spin.jpg",
+        stationLocation: "Embarcadero",
+        artistName: `Robert Ellison`,
+        location: "Concourse - Main Level"
+    },
+    {
+        name: "Wall Canyon",
+        image: "wall-canyon.jpg",
+        stationLocation: "Embarcadero",
+        artistName: `Stephen de Staebler`,
+        location: "Concourse - Main Level"
+    },
 ];
 
 export default bartArtData;
